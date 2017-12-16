00becf311590d335d9720f729e4d31f0d8a24001
jonathanh <jonforest@gmail.com>
2017-12-16 20:35:18 +1300

    Basic time row implemented, without actions/events

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, TextInput, View, FlatList, Button} from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // todo: load the state from the local storage, whatever that is
+    // todo: work out the state structure for this app
+    this.state = {
+     timelogItems: [
+       {key: 'Work for Dennis', time: 6000, started: false}
+     ]
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{padding: 50}}>
+        <FlatList
+          data={this.state.timelogItems}
+          renderItem={({item}) => <TimeRecord record={item}/>}
+        />
+      </View>
+    );
+  }
+}
+
+class TimeRecord extends React.Component {
+  time() {
+    const totalSeconds = this.props.record.time;
+    const hours = Math.floor(totalSeconds / 3600);
+    const hoursRemainder = totalSeconds % 3600;
+    const minutes = Math.floor(hoursRemainder / 60);
+    const minutesRemainder = hoursRemainder / 60;
+    const seconds = Math.floor(minutesRemainder);
+    return `${hours}:${minutes}:${seconds}`;
+  }
+
+  noop() {
+    return;
+  }
+
+  render() {
+    const button = this.props.record.started
+      ? <Button title="Stop" onPress={this.noop} style={{flex:1}} />
+      : <Button title="Start" onPress={this.noop} style={{flex:1}} />
+    return (
+      <View style={{flex: 0.5, flexDirection: 'row'}}>
+        <Text style={{flex: 1}}>{this.props.record.key}</Text>
+        <Text style={{flex: 1}}>{this.time()}</Text>
+        {button}
       </View>
     );
   }
